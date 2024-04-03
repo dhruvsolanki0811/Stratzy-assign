c8fc898ea63b25a59e60264dbb6a4ff59d6f655e
Dhruv Solanki <95128593+dhruvsolanki0811@users.noreply.github.com>
2024-04-03 17:44:58 +0530

    Refactored Height of Card'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import SideBarCard from "@/components/SideBarCard/SideBarCard";
 export default function Home() {
   return (
     <>
-      <div className="content-wrapper w-[100vw] h-[100vh] flex  justify-center">
+      <div className="content-wrapper w-[100vw]  flex  justify-center">
         <SideBarCard></SideBarCard>
       </div>
     </>
